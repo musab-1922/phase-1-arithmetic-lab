de0df82a365dad06e0a388856ec7d455e8421102
musab <musababdi1922@gmail.com>
2023-03-18 23:20:51 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.floor(Math.random() * 100) + 1;
+
+let dividend = 27;
+let divisor = 5;
+let mod = dividend % divisor; // mod is equal to 2
+if (mod === 2) {
+  mod += 2; // add 2 to mod to make it equal to 4
+}
+
+
+let set = [10, 5, 20, 15, 2];
+let max = Math.max(...set); // returns 20
+
+
+
+
+
+
